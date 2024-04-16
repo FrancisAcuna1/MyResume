93bd11480327d41f11a17bc3e6396f981137fdf8
FrancisAcuna1 <francisacuna@thelewiscollege.edu.ph>
2024-04-16 17:57:32 +0800

    first commit

---
+++ nav.js
@@ -0,0 +1,25 @@
+window.onscroll = function (event) {
+    var scroll = window.pageYOffset;
+    if (scroll > 100) {
+      document.getElementById('navbar1').style ="background-color: white;"         
+    }
+    else{
+      document.getElementById('navbar1').style = ""
+    }
+    }
+
+    var toastbutton = document.getElementById('hirebtn');
+    toastbutton.addEventListener('click', function(){
+      toastr.success('Your Request Successfully Send!', 'Success', {
+        progressBar: true,
+        textcolor: 'black',
+        position: 'relative',
+        zIndex: 1,
+        // backgroundColor: 'black'
+      });
+    });
+    
+
+// function showalert(){
+//   alert('Your Request was sending Successfully!');
+// }

90516b030621aedf4581b1adce7f70ebd095bdaa
FrancisAcuna1 <francisacuna@thelewiscollege.edu.ph>
2024-04-19 17:13:25 +0800

    final na ini

--- nav.js
+++ nav.js
@@ -1,12 +1,17 @@
-window.onscroll = function (event) {
-    var scroll = window.pageYOffset;
-    if (scroll > 100) {
-      document.getElementById('navbar1').style ="background-color: white;"         
-    }
-    else{
-      document.getElementById('navbar1').style = ""
-    }
-    }
+document.addEventListener("DOMContentLoaded", function() {
+  const navbar = document.getElementById("navbar1");
+
+  window.addEventListener("scroll", function() {
+      if (window.scrollY > 50) {
+          navbar.classList.remove("transparent-navbar");
+          navbar.classList.add("solid-navbar");
+      } else {
+          navbar.classList.remove("solid-navbar");
+          navbar.classList.add("transparent-navbar");
+      }
+  });
+});
+
 
     var toastbutton = document.getElementById('hirebtn');
     toastbutton.addEventListener('click', function(){
